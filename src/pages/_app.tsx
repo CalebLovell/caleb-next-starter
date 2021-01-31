3fc4f864b3517432ce056b6171aca0605347bf6e
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2021-01-31 01:51:55 -0700

    add react query and toasts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,21 @@
 import '../styles/global.css';
 
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 import { ThemeProvider } from 'next-themes';
+import { ToastProvider } from 'react-toast-notifications';
+
+const queryClient = new QueryClient();
 
 const MyApp = ({ Component, pageProps }) => {
 	return (
-		<ThemeProvider attribute='class'>
-			<Component {...pageProps} />
-		</ThemeProvider>
+		<QueryClientProvider client={queryClient}>
+			<ThemeProvider attribute='class'>
+				<ToastProvider>
+					<Component {...pageProps} />
+				</ToastProvider>
+			</ThemeProvider>
+		</QueryClientProvider>
 	);
 };
 
