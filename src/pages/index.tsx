5539262f0feedb9510b9a0142388e8162e9ef729
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2021-01-31 02:37:52 -0700

    update link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 const IndexPage = (): JSX.Element => {
 	return (
-		<main className='flex items-center justify-center min-h-screen'>
+		<main className='flex items-center justify-center min-h-screen m-2'>
 			<div className='flex flex-col text-center'>
-				<h1 className='text-3xl'>Caleb&apos;s Next.js starter template, built with TailwindCSS, React Query and TypeScript.</h1>
-				<h2 className='mt-3 text-lg'>
-					<a href=''>Click here to check out the README on Github</a>
+				<h1 className='text-3xl'>Caleb&apos;s Next.js Starter Template, built with Tailwind, TypeScript, React Query, etc.</h1>
+				<h2 className='mt-2 text-lg'>
+					<a href='https://github.com/CalebLovell/caleb-next-starter' target='_blank' rel='noreferrer'>
+						Click here to check out the repository on Github
+					</a>
 				</h2>
 			</div>
 		</main>
