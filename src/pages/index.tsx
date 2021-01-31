5efd40443eed7ab9b05e3925937a3933db469a74
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2021-01-31 02:19:32 -0700

    add styling and scripts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
 const IndexPage = (): JSX.Element => {
-	return <div>Caleb&apos;s Next.js starter template, built with TailwindCSS, React Query and TypeScript.</div>;
+	return (
+		<main className='flex items-center justify-center min-h-screen'>
+			<div className='flex flex-col text-center'>
+				<h1 className='text-3xl'>Caleb&apos;s Next.js starter template, built with TailwindCSS, React Query and TypeScript.</h1>
+				<h2 className='mt-3 text-lg'>
+					<a href=''>Click here to check out the README on Github</a>
+				</h2>
+			</div>
+		</main>
+	);
 };
 
 export default IndexPage;
