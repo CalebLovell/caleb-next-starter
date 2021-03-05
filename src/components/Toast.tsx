9c402641a091bbc1cfc82d7045525c254444e62c
Caleb Lovell <caleblovell1@gmail.com>
2021-03-05 01:59:11 -0700

    fix toasts so classes aren't purged

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -14,22 +14,18 @@ export const Toast: React.FC<ToastProps> = ({ appearance, onDismiss, children })
 		>
 			<div
 				role='status'
-				className={`flex justify-between items-center my-2 rounded-lg border-l-4 border-${renderColor(appearance)}-400 bg-${renderColor(
-					appearance
-				)}-100`}
+				className={`flex justify-between items-center my-2 rounded-lg border-l-4 ${getBorderColor400(appearance)} ${getBgColor100(appearance)}`}
 			>
 				<div className='flex items-center py-2 pl-4'>
 					<div>{renderIcon(appearance)}</div>
-					<p className={`mx-4 font-medium text-${renderColor(appearance)}-400`}>{children}</p>
+					<p className={`mx-4 font-medium ${getTextColor400(appearance)}`}>{children}</p>
 				</div>
 				<div className='py-2 pr-4'>
 					<button
 						onClick={() => onDismiss()}
-						className={`p-1 bg-${renderColor(appearance)}-100 rounded-md text-${renderColor(appearance)}-500 hover:bg-${renderColor(
+						className={`p-1 ${getBgColor100(appearance)} rounded-md ${getTextColor500(appearance)} ${getHoverBgColor100(
 							appearance
-						)}-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-${renderColor(
-							appearance
-						)}-100 focus:ring-${renderColor(appearance)}-600`}
+						)} focus:outline-none focus:ring-2 focus:ring-offset-2 ${getFocusRingColor100(appearance)} ${getFocusRingColor600(appearance)}`}
 					>
 						<span className='sr-only'>Dismiss</span>
 						<svg className='w-6 h-6' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor' aria-hidden='true'>
@@ -46,16 +42,94 @@ export const Toast: React.FC<ToastProps> = ({ appearance, onDismiss, children })
 	);
 };
 
-const renderColor = appearance => {
+const getBgColor100 = appearance => {
 	switch (appearance) {
 		case `info`:
-			return `blue`;
+			return `bg-blue-100`;
 		case `success`:
-			return `green`;
+			return `bg-green-100`;
 		case `warning`:
-			return `yellow`;
+			return `bg-yellow-100`;
 		case `error`:
-			return `red`;
+			return `bg-red-100`;
+	}
+};
+
+const getHoverBgColor100 = appearance => {
+	switch (appearance) {
+		case `info`:
+			return `hover:bg-blue-100`;
+		case `success`:
+			return `hover:bg-green-100`;
+		case `warning`:
+			return `hover:bg-yellow-100`;
+		case `error`:
+			return `hover:bg-red-100`;
+	}
+};
+
+const getFocusRingColor100 = appearance => {
+	switch (appearance) {
+		case `info`:
+			return `focus:ring-offset-bg-blue-100`;
+		case `success`:
+			return `focus:ring-offset-bg-green-100`;
+		case `warning`:
+			return `focus:ring-offset-bg-yellow-100`;
+		case `error`:
+			return `focus:ring-offset-bg-red-100`;
+	}
+};
+
+const getFocusRingColor600 = appearance => {
+	switch (appearance) {
+		case `info`:
+			return `focus:ring-offset-bg-blue-600`;
+		case `success`:
+			return `focus:ring-offset-bg-green-600`;
+		case `warning`:
+			return `focus:ring-offset-bg-yellow-600`;
+		case `error`:
+			return `focus:ring-offset-bg-red-600`;
+	}
+};
+
+const getBorderColor400 = appearance => {
+	switch (appearance) {
+		case `info`:
+			return `border-blue-400`;
+		case `success`:
+			return `border-green-400`;
+		case `warning`:
+			return `border-yellow-400`;
+		case `error`:
+			return `border-red-400`;
+	}
+};
+
+const getTextColor400 = appearance => {
+	switch (appearance) {
+		case `info`:
+			return `text-blue-400`;
+		case `success`:
+			return `text-green-400`;
+		case `warning`:
+			return `text-yellow-400`;
+		case `error`:
+			return `text-red-400`;
+	}
+};
+
+const getTextColor500 = appearance => {
+	switch (appearance) {
+		case `info`:
+			return `text-blue-500`;
+		case `success`:
+			return `text-green-500`;
+		case `warning`:
+			return `text-yellow-500`;
+		case `error`:
+			return `text-red-500`;
 	}
 };
 
@@ -64,7 +138,7 @@ const renderIcon = appearance => {
 		case `info`:
 			return (
 				<svg
-					className={`w-6 h-6 text-${renderColor(appearance)}-400`}
+					className={`w-6 h-6 ${getTextColor400(appearance)}`}
 					xmlns='http://www.w3.org/2000/svg'
 					viewBox='0 0 20 20'
 					fill='currentColor'
@@ -80,7 +154,7 @@ const renderIcon = appearance => {
 		case `success`:
 			return (
 				<svg
-					className={`w-6 h-6 text-${renderColor(appearance)}-400`}
+					className={`w-6 h-6 ${getTextColor400(appearance)}`}
 					xmlns='http://www.w3.org/2000/svg'
 					viewBox='0 0 20 20'
 					fill='currentColor'
@@ -96,7 +170,7 @@ const renderIcon = appearance => {
 		case `warning`:
 			return (
 				<svg
-					className={`w-6 h-6 pt-0.5 text-${renderColor(appearance)}-400`}
+					className={`w-6 h-6 pt-0.5 ${getTextColor400(appearance)}`}
 					xmlns='http://www.w3.org/2000/svg'
 					viewBox='0 0 20 20'
 					fill='currentColor'
@@ -112,7 +186,7 @@ const renderIcon = appearance => {
 		case `error`:
 			return (
 				<svg
-					className={`w-6 h-6 text-${renderColor(appearance)}-400`}
+					className={`w-6 h-6 ${getTextColor400(appearance)}`}
 					xmlns='http://www.w3.org/2000/svg'
 					viewBox='0 0 20 20'
 					fill='currentColor'
--- src/components/ToastContainer.tsx
+++ src/components/ToastContainer.tsx
@@ -2,12 +2,7 @@ import { ToastContainerProps } from 'react-toast-notifications';
 
 export const ToastContainer: React.FC<ToastContainerProps> = ({ hasToasts, children }): JSX.Element => {
 	return (
-		<div
-			className='container fixed bottom-0 z-50 max-h-full pb-2 overflow-hidden'
-			style={{
-				pointerEvents: hasToasts ? `auto` : `none`,
-			}}
-		>
+		<div className={`container fixed bottom-0 z-50 max-h-full pb-2 overflow-hidden ${hasToasts ? `pointer-events-auto` : `pointer-events-none`}`}>
 			{children}
 		</div>
 	);
