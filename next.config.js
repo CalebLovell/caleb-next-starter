74f17806de347fbacb6f832d5d09c71ac9b17951
Caleb Lovell <caleblovell1@gmail.com>
2021-03-10 02:30:12 -0700

    add MDX POC. Still a WIP!

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { i18n } = require(`./next-i18next.config`);
+const withMDX = require(`@next/mdx`)({
+	extension: /\.mdx?$/,
+});
 
-module.exports = {
+module.exports = withMDX({
 	i18n,
-};
+	pageExtensions: [`js`, `jsx`, `ts`, `tsx`, `md`, `mdx`],
+});
